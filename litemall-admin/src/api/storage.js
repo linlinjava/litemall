0bb0441385e955a6701dc53217e684c848b42d2e
Junling Bu <linlinjavaer@gmail.com>
2018-04-17 15:07:00 +0800

    update[litemall-admin]: 访问存储服务超时时，显示警告信息，可能是存储服务没有启动。

--- litemall-admin/src/api/storage.js
+++ litemall-admin/src/api/storage.js
@@ -1,10 +1,25 @@
 import axios from 'axios'
+import { Message } from 'element-ui'
 
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.OS_API, // api的base_url
   timeout: 5000 // request timeout
 })
+
+// respone interceptor
+service.interceptors.response.use(
+  response => {
+    return response
+  }, error => {
+    console.log('err' + error)// for debug
+    Message({
+      message: '对象存储服务访问超时，请检查链接是否能够访问。',
+      type: 'error',
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  })
 
 export function listStorage(query) {
   return service({
