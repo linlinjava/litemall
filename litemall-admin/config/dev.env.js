25989938bb20b8380936e01dd05ed22e72b2a027
Junling Bu <linlinjavaer@gmail.com>
2018-04-21 19:37:27 +0800

    update[litemall-admin]: 调整对象存储服务地址。

--- litemall-admin/config/dev.env.js
+++ litemall-admin/config/dev.env.js
@@ -2,5 +2,5 @@ module.exports = {
 	NODE_ENV: '"development"',
 	ENV_CONFIG: '"dev"',
   BASE_API: '"http://localhost:8083/admin"',
-  OS_API: '"http://localhost:8081/storage"'
+  OS_API: '"http://localhost:8081/os"'
 }
--- litemall-admin/config/prod.env.js
+++ litemall-admin/config/prod.env.js
@@ -2,5 +2,5 @@ module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"prod"',
   BASE_API: '"http://122.152.206.172:8083/admin"',
-  OS_API: '"http://122.152.206.172:8081/storage"'
+  OS_API: '"http://122.152.206.172:8081/os"'
 }
