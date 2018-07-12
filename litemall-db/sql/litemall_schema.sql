6947d1743fb46a312309ca47093331e5a91f68b6
huanggusheng <huanggusheng520@sina.com>
2018-07-12 10:33:35 +0800

    test git

--- litemall-db/sql/litemall_schema.sql
+++ litemall-db/sql/litemall_schema.sql
@@ -1,7 +1,15 @@
-drop database if exists litemall;
-drop user if exists 'litemall'@'localhost';
-create database litemall;
-use litemall;
-create user 'litemall'@'localhost' identified by 'litemall123456';
-grant all privileges on litemall.* to 'litemall'@'localhost';
+# drop database if exists litemall;
+# drop user if exists 'litemall'@'localhost';
+# create database litemall;
+# use litemall;
+# create user 'litemall'@'localhost' identified by 'litemall123456';
+# grant all privileges on litemall.* to 'litemall'@'localhost';
+# flush privileges;
+
+drop database if exists beef;
+drop user if exists 'beef'@'%';
+create database beef;
+use beef;
+create user 'beef'@'%' identified by 'beef123';
+grant all privileges on beef.* to 'beef'@'%';
 flush privileges;
