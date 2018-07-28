7676cdcdffa925c857a0c3de83911cd056dfabd5
Junling Bu <linlinjavaer@gmail.com>
2018-07-28 15:48:14 +0800

    litemall-admin和litemall-wx的图片上传不再依赖os模块，而是litemall-admin-api和litemall-wx-api模块。

--- litemall-admin/config/prod.env.js
+++ litemall-admin/config/prod.env.js
@@ -1,6 +1,5 @@
 module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"prod"',
-    BASE_API: '"https://www.example.com/admin"',
-    OS_API: '"https://www.example.com/os"'
+    BASE_API: '"https://www.example.com/admin"'
 }
