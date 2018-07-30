0d856849ec3ca3b96d4ae66d9cede8a8ab1b2f44
Junling Bu <linlinjavaer@gmail.com>
2018-07-30 13:52:37 +0800

    调整litemall-wx和litemall-admin的默认配置端口到8080

--- litemall-admin/config/dev.env.js
+++ litemall-admin/config/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
 	NODE_ENV: '"development"',
-	ENV_CONFIG: '"dev"',
-    BASE_API: '"http://localhost:8083/admin"'
+  ENV_CONFIG: '"dev"',
+  BASE_API: '"http://localhost:8080/admin"'
 }
