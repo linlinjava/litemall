58fc87d2f061f108ad27028caf0a2d2a8e1ef6e3
Junling Bu <linlinjavaer@gmail.com>
2018-08-02 14:53:03 +0800

    fix[litemall-wx-api]: 设置默认值防止空指针异常

--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxGoodsController.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxGoodsController.java
@@ -230,7 +230,8 @@ public class WxGoodsController {
                        @LoginUser Integer userId,
                        @RequestParam(defaultValue = "1") Integer page,
                        @RequestParam(defaultValue = "10") Integer size,
-                       @Sort String sort, @Order String order) {
+                       @Sort @RequestParam(defaultValue = "add_time") String sort,
+                       @Order @RequestParam(defaultValue = "desc") String order){
 
         //添加到搜索历史
         if (userId != null && !StringUtils.isNullOrEmpty(keyword)) {
