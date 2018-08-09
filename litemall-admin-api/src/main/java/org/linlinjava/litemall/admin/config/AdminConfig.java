ebc1fe35c201f8c595bdcf1a1d238d5081bf6eba
Junling Bu <linlinjavaer@gmail.com>
2018-08-09 10:47:47 +0800

    chore[litemall-admin-api, litemall-wx-api]: 修复升级spring boot 2.0带来的warning

--- litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/config/AdminConfig.java
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/config/AdminConfig.java
@@ -3,12 +3,11 @@ package org.linlinjava.litemall.admin.config;
 import org.linlinjava.litemall.admin.annotation.support.LoginAdminHandlerMethodArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
-public class AdminConfig extends WebMvcConfigurerAdapter {
+public class AdminConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new LoginAdminHandlerMethodArgumentResolver());
--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/config/UseConfig.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/config/UseConfig.java
@@ -3,12 +3,11 @@ package org.linlinjava.litemall.wx.config;
 import org.linlinjava.litemall.wx.annotation.support.LoginUserHandlerMethodArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
-public class UseConfig extends WebMvcConfigurerAdapter {
+public class UseConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new LoginUserHandlerMethodArgumentResolver());
