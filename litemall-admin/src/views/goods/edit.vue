420650e86d3843f11167263fc531b45d8bb66a73
Junling Bu <linlinjavaer@gmail.com>
2018-10-09 11:17:24 +0800

    fix[litemall-admin]: 修复图片上传地址返回相对链接。

--- litemall-admin/src/views/goods/edit.vue
+++ litemall-admin/src/views/goods/edit.vue
@@ -305,6 +305,7 @@ export default {
       },
       editorInit: {
         language: 'zh_CN',
+        convert_urls: false,
         plugins: ['advlist anchor autolink autoresize autosave emoticons fullscreen hr image imagetools importcss insertdatetime legacyoutput link lists media nonbreaking noneditable pagebreak paste preview print save searchreplace tabfocus table template textcolor textpattern visualblocks visualchars wordcount'],
         toolbar: ['bold italic underline strikethrough alignleft aligncenter alignright outdent indent  blockquote undo redo removeformat subscript superscript ', 'hr bullist numlist link image charmap preview anchor pagebreak fullscreen media table emoticons forecolor backcolor'],
         images_upload_handler: function(blobInfo, success, failure) {
