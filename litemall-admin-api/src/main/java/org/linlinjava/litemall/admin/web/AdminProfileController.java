88c5a81e3edb8b549e0ceb74a644af8b7dd1f34f
Junling Bu <linlinjavaer@gmail.com>
2018-11-27 20:15:07 +0800

    chore[litemall-admin-api]: 禁止超级管理员修改密码，防止演示平台不能访问。

--- litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/web/AdminProfileController.java
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/web/AdminProfileController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.linlinjava.litemall.admin.util.AdminResponseCode.ADMIN_ALTER_NOT_ALLOWED;
 import static org.linlinjava.litemall.admin.util.AdminResponseCode.ADMIN_INVALID_ACCOUNT;
 
 @RestController
@@ -31,6 +32,12 @@ public class AdminProfileController {
     public Object create(@LoginAdmin Integer adminId, @RequestBody String body) {
         if (adminId == null) {
             return ResponseUtil.unlogin();
+        }
+
+        // TODO 这里开发者需要删除以下检验代码
+        // 目前这里不允许修改超级管理员是防止演示平台上他人修改管理员密码而导致登录失败
+        if (adminId == 1) {
+            return ResponseUtil.fail(ADMIN_ALTER_NOT_ALLOWED, "超级管理员不能修改");
         }
 
         String oldPassword = JacksonUtil.parseString(body, "oldPassword");
