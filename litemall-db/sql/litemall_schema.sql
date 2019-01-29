50028820fe4e957de506e214a15fdbea844f43ea
DongGuoChao <blackdog1987@gmail.com>
2019-01-29 10:44:12 +0800

    support emoji

--- litemall-db/sql/litemall_schema.sql
+++ litemall-db/sql/litemall_schema.sql
@@ -1,6 +1,7 @@
 drop database if exists litemall;
 drop user if exists 'litemall'@'localhost';
-create database litemall;
+-- 支持emoji：需要mysql数据库参数： character_set_server=utf8mb4
+create database litemall default character set utf8mb4 collate utf8mb4_unicode_ci;
 use litemall;
 create user 'litemall'@'localhost' identified by 'litemall123456';
 grant all privileges on litemall.* to 'litemall'@'localhost';
