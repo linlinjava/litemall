8219d12fdbda012eda9033ff5ceab1959f6376a8
Junling Bu <linlinjavaer@gmail.com>
2019-03-27 16:56:27 +0800

    chore: 脚本完善启动

--- deploy/bin/deploy.sh
+++ deploy/bin/deploy.sh
@@ -3,4 +3,6 @@
 # 本脚本的作用是停止当前Spring Boot应用，然后再次部署
 sudo service litemall stop
 sudo ln -f -s /home/ubuntu/deploy/litemall/litemall.jar /etc/init.d/litemall
+sudo update-rc.d litemall defaults
+sudo update-rc.d litemall enable
 sudo service litemall start
