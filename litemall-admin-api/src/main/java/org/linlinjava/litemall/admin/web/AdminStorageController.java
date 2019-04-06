fd80760bb5e8f949b02e90ea60ee39941d81791a
Junling Bu <linlinjavaer@gmail.com>
2019-04-06 23:10:55 +0800

    fix[litemall-admin-api, litemall-core]: 对象存储返回对象存储信息，而不仅仅是图片链接

--- litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/web/AdminStorageController.java
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/web/AdminStorageController.java
@@ -56,10 +56,8 @@ public class AdminStorageController {
     @PostMapping("/create")
     public Object create(@RequestParam("file") MultipartFile file) throws IOException {
         String originalFilename = file.getOriginalFilename();
-        String url = storageService.store(file.getInputStream(), file.getSize(), file.getContentType(), originalFilename);
-        Map<String, Object> data = new HashMap<>();
-        data.put("url", url);
-        return ResponseUtil.ok(data);
+        LitemallStorage litemallStorage = storageService.store(file.getInputStream(), file.getSize(), file.getContentType(), originalFilename);
+        return ResponseUtil.ok(litemallStorage);
     }
 
     @RequiresPermissions("admin:storage:read")
