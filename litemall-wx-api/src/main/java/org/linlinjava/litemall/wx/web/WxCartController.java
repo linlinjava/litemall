b252cf2b5bf5b0a5f43aa4d71f064dc5bb107335
Junling Bu <linlinjavaer@gmail.com>
2019-04-16 17:40:43 +0800

    fix[litemall-wx-api]: 下单时存在传递不合适的优惠券的情况，此时后端自动选择合适的优惠券。

--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxCartController.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxCartController.java
@@ -466,11 +466,14 @@ public class WxCartController {
         }
         else {
             LitemallCoupon coupon = couponVerifyService.checkCoupon(userId, couponId, checkedGoodsPrice);
-            // 用户选择的优惠券有问题
+            // 用户选择的优惠券有问题，则选择合适优惠券，否则使用用户选择的优惠券
             if(coupon == null){
-                return ResponseUtil.badArgumentValue();
+                couponPrice = tmpCouponPrice;
+                couponId = tmpCouponId;
             }
-            couponPrice = coupon.getDiscount();
+            else {
+                couponPrice = coupon.getDiscount();
+            }
         }
 
         // 根据订单商品总价计算运费，满88则免运费，否则8元；
