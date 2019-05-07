ca4835b28f0e388a7484fa340a2d598b2df6aa69
Junling Bu <linlinjavaer@gmail.com>
2019-05-07 14:45:57 +0800

    chore[litemall-wx, litemall-wx-api]: 团购API调整及文档

---
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/service/WxGrouponRuleService.java
@@ -0,0 +1,64 @@
+package org.linlinjava.litemall.wx.service;
+
+import com.github.pagehelper.Page;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.linlinjava.litemall.db.domain.LitemallGoods;
+import org.linlinjava.litemall.db.domain.LitemallGrouponRules;
+import org.linlinjava.litemall.db.service.LitemallGoodsService;
+import org.linlinjava.litemall.db.service.LitemallGrouponRulesService;
+import org.linlinjava.litemall.db.service.LitemallGrouponService;
+import org.linlinjava.litemall.wx.vo.GrouponRuleVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WxGrouponRuleService {
+    private final Log logger = LogFactory.getLog(WxGrouponRuleService.class);
+
+    @Autowired
+    private LitemallGrouponRulesService grouponRulesService;
+    @Autowired
+    private LitemallGrouponService grouponService;
+    @Autowired
+    private LitemallGoodsService goodsService;
+
+
+    public List<GrouponRuleVo> queryList(Integer page, Integer size) {
+        return queryList(page, size, "add_time", "desc");
+    }
+
+
+    public List<GrouponRuleVo> queryList(Integer page, Integer size, String sort, String order) {
+        Page<LitemallGrouponRules> grouponRulesList = (Page)grouponRulesService.queryList(page, size, sort, order);
+
+        Page<GrouponRuleVo> grouponList = new Page<GrouponRuleVo>();
+        grouponList.setPages(grouponRulesList.getPages());
+        grouponList.setPageNum(grouponRulesList.getPageNum());
+        grouponList.setPageSize(grouponRulesList.getPageSize());
+        grouponList.setTotal(grouponRulesList.getTotal());
+
+        for (LitemallGrouponRules rule : grouponRulesList) {
+            Integer goodsId = rule.getGoodsId();
+            LitemallGoods goods = goodsService.findById(goodsId);
+            if (goods == null)
+                continue;
+
+            GrouponRuleVo grouponRuleVo = new GrouponRuleVo();
+            grouponRuleVo.setId(goods.getId());
+            grouponRuleVo.setName(goods.getName());
+            grouponRuleVo.setBrief(goods.getBrief());
+            grouponRuleVo.setPicUrl(goods.getPicUrl());
+            grouponRuleVo.setCounterPrice(goods.getCounterPrice());
+            grouponRuleVo.setRetailPrice(goods.getRetailPrice());
+            grouponRuleVo.setGrouponPrice(goods.getRetailPrice().subtract(rule.getDiscount()));
+            grouponRuleVo.setGrouponDiscount(rule.getDiscount());
+            grouponRuleVo.setGrouponMember(rule.getDiscountMember());
+            grouponList.add(grouponRuleVo);
+        }
+
+        return grouponList;
+    }
+}
