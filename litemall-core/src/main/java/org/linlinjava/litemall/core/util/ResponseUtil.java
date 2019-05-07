4d28bc2d1b9588334ca16eaa51702bcb8a7fcecd
Junling Bu <linlinjavaer@gmail.com>
2019-05-07 10:28:56 +0800

    feat[litemall-core]: 数组数据进一步增加分页信息

--- litemall-core/src/main/java/org/linlinjava/litemall/core/util/ResponseUtil.java
+++ litemall-core/src/main/java/org/linlinjava/litemall/core/util/ResponseUtil.java
@@ -62,9 +62,36 @@ public class ResponseUtil {
         if (list instanceof Page) {
             Page page = (Page) list;
             data.put("total", page.getTotal());
+            data.put("page", page.getPageNum());
+            data.put("limit", page.getPageSize());
+            data.put("pages", page.getPages());
         }
         else{
             data.put("total", list.size());
+            data.put("page", 1);
+            data.put("limit", list.size());
+            data.put("pages", 1);
+        }
+
+        return ok(data);
+    }
+
+    public static Object okList(List list, List pagedList) {
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("list", list);
+
+        if (pagedList instanceof Page) {
+            Page page = (Page) pagedList;
+            data.put("total", page.getTotal());
+            data.put("page", page.getPageNum());
+            data.put("limit", page.getPageSize());
+            data.put("pages", page.getPages());
+        }
+        else{
+            data.put("total", pagedList.size());
+            data.put("page", 1);
+            data.put("limit", pagedList.size());
+            data.put("pages", 1);
         }
 
         return ok(data);
