d8ccd137515de584894555dcd688033988f99c51
Junling Bu <linlinjavaer@gmail.com>
2019-11-13 19:41:12 +0800

    feat[litemall-core]: 支持延迟队列任务

---
+++ litemall-core/src/main/java/org/linlinjava/litemall/core/task/TaskService.java
@@ -0,0 +1,47 @@
+package org.linlinjava.litemall.core.task;
+
+import com.sun.org.apache.bcel.internal.generic.ARETURN;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Iterator;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Executors;
+
+
+@Component
+public class TaskService {
+    private TaskService taskService;
+    private DelayQueue<Task> delayQueue =  new DelayQueue<Task>();
+
+    @PostConstruct
+    private void init() {
+        taskService = this;
+
+        Executors.newSingleThreadExecutor().execute(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Task task = delayQueue.take();
+                        task.run();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    public void addTask(Task task){
+        if(delayQueue.contains(task)){
+            return;
+        }
+        delayQueue.add(task);
+    }
+
+    public void removeTask(Task task){
+        delayQueue.remove(task);
+    }
+
+}
