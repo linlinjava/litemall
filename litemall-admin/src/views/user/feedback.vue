e7d85dabef38d63b61fc29c0035e70f74307323c
Junling Bu <linlinjavaer@gmail.com>
2019-11-21 22:38:49 +0800

    feat[litemall-admin]: 在意见反馈和商品评论页面使用el-image组件，可以放大。

--- litemall-admin/src/views/user/feedback.vue
+++ litemall-admin/src/views/user/feedback.vue
@@ -24,7 +24,7 @@
 
       <el-table-column align="center" label="反馈图片" prop="picUrls">
         <template slot-scope="scope">
-          <img v-for="item in scope.row.picUrls" :key="item" :src="item" width="40">
+          <el-image v-for="item in scope.row.picUrls" :key="item" :src="item" :preview-src-list="scope.row.picUrls" :lazy="true" style="width: 40px; height: 40px; margin-right: 5px;"/>
         </template>
       </el-table-column>
 
