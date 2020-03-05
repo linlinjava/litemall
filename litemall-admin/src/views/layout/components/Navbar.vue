7a34bbe64ef0c7d7857a3cff5f6b88d8657cc7a7
Junling Bu <linlinjavaer@gmail.com>
2020-03-05 21:00:52 +0800

    per[litemall-admin]: 退出优化。

--- litemall-admin/src/views/layout/components/Navbar.vue
+++ litemall-admin/src/views/layout/components/Navbar.vue
@@ -85,7 +85,7 @@ export default {
     },
     logout() {
       this.$store.dispatch('LogOut').then(() => {
-        location.reload()// In order to re-instantiate the vue-router object to avoid bugs
+        this.$router.push(`/login?redirect=${this.$route.fullPath}`)
       })
     }
   }
