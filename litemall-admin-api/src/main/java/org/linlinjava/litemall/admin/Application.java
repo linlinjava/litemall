e1dfa1e4a2950d491af554e1e8929b30e49f5023
Junling Bu <linlinjavaer@gmail.com>
2020-06-01 19:39:07 +0800

    chore

--- litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/Application.java
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(scanBasePackages = {"org.linlinjava.litemall.db", "org.linlinjava.litemall.core", "org" +
-        ".linlinjava.litemall.admin"})
+@SpringBootApplication(scanBasePackages = {"org.linlinjava.litemall.db", "org.linlinjava.litemall.core",
+        "org.linlinjava.litemall.admin"})
 @MapperScan("org.linlinjava.litemall.db.dao")
 @EnableTransactionManagement
 @EnableScheduling
