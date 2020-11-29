f50138243c9bfc94a65a5993e425d78acbb8bad6
linlinjava <linlinjavaer@gmail.com>
2020-11-29 11:12:07 +0800

    chore

--- deploy/bin/deploy.sh
+++ deploy/bin/deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # 本脚本的作用是停止当前Spring Boot应用，然后再次部署
-sudo service litemall stop
-sudo ln -f -s /home/ubuntu/deploy/litemall/litemall.jar /etc/init.d/litemall
-sudo update-rc.d litemall defaults
-sudo update-rc.d litemall enable
-sudo service litemall start
+PID=$(ps -ef | grep litemall.jar | grep -v grep | awk '{ print $2 }')
+
+if [ ! -z "$PID" ]
+then
+    kill $PID
+fi
+
+cd /home/ubuntu/deploy/litemall || exit 2
+nohup java -jar litemall.jar > log.log 2>&1 &
