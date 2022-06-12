5f6e76d7fc5e2b37f72258d38698d6c71ead38fe
linlinjava <linlinjavaer@gmail.com>
2022-06-12 19:03:40 +0800

    fix[litemall-admin-api]: 修复setSessionIdUrlRewritingEnabled设置失效

--- litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/shiro/AdminWebSessionManager.java
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/shiro/AdminWebSessionManager.java
@@ -29,6 +29,8 @@ public class AdminWebSessionManager extends DefaultWebSessionManager {
             request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_SOURCE, REFERENCED_SESSION_ID_SOURCE);
             request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID, id);
             request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_IS_VALID, Boolean.TRUE);
+            // 参考 https://blog.csdn.net/narutots/article/details/120363843
+            request.setAttribute(ShiroHttpServletRequest.SESSION_ID_URL_REWRITING_ENABLED, this.isSessionIdUrlRewritingEnabled());
             return id;
         } else {
             return super.getSessionId(request, response);
