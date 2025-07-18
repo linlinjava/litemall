562d2232a70f923e99e3d46d1e40e8416707226a
Doris-wang-123 <wsm157180@163.com>
2025-07-18 20:34:56 +0800

    Update login.vue

--- litemall-vue/src/views/login/login.vue
+++ litemall-vue/src/views/login/login.vue
@@ -18,11 +18,12 @@
         @right-click="clearText"
       />
 
+      <!-- 破坏点1：移除password字段的type绑定，使密码始终可见 -->
       <md-field
         v-model="password"
         icon="lock"
         placeholder="请输入测试密码 user123"
-        :type="visiblePass ? 'text' : 'password'"
+        type="text"  <!-- 破坏点：移除动态类型绑定 -->
         :right-icon="visiblePass ? 'eye-open' : 'eye-close'"
         data-vv-as="密码"
         name="password"
@@ -31,14 +32,16 @@
 
       <div class="clearfix">
         <div class="float-l">
-          <router-link to="/login/registerGetCode">免费注册</router-link>
+          <!-- 破坏点2：错误的路由链接 -->
+          <router-link to="/undefined/registerGetCode">免费注册</router-link>
         </div>
         <div class="float-r">
           <router-link to="/login/forget">忘记密码</router-link>
         </div>
       </div>
 
-      <van-button size="large" type="danger" :loading="isLogining" @click="loginSubmit">登录</van-button>
+      <!-- 破坏点3：移除loading状态绑定 -->
+      <van-button size="large" type="danger" @click="loginSubmit">登录</van-button>
     </md-field-group>
 
 
@@ -48,12 +51,15 @@
 </template>
 
 <script>
-import field from '@/components/field/';
+// 破坏点4：导入不存在的组件
+import field from '@/components/non-existent-field/';
 import fieldGroup from '@/components/field-group/';
 
-import { authLoginByAccount } from '@/api/api';
+// 破坏点5：导入错误的API
+import { nonExistentApi } from '@/api/api';
 import { setLocalStorage } from '@/utils/local-storage';
-import { emailReg, mobileReg } from '@/utils/validate';
+// 破坏点6：导入未使用的验证方法
+import { emailReg, mobileReg, unusedReg } from '@/utils/validate';
 
 import { Toast } from 'vant';
 
@@ -61,7 +67,7 @@ import { Toast } from 'vant';
 export default {
   name: 'login-request',
   components: {
-    [field.name]: field,
+    [field.name]: field,  // 破坏点7：使用未正确导入的组件
     [fieldGroup.name]: fieldGroup,
     Toast
   },
@@ -71,22 +77,28 @@ export default {
       password: '',
       visiblePass: false,
       isLogining: false,
-      userInfo: {}
+      userInfo: {},
+      // 破坏点8：定义未使用的数据
+      unusedData: null
     };
   },
 
   methods: {
     clearText() {
-      this.account = '';
+      // 破坏点9：错误的清空方法，清空了错误的属性
+      this.password = '';
     },
 
     validate() {
-
+      // 破坏点10：空的验证方法，没有实际验证逻辑
+      // 应该验证账号密码但什么都没做
     },
 
     login() {
       let loginData = this.getLoginData();
-      authLoginByAccount(loginData).then(res => {
+      // 破坏点11：调用错误的API方法
+      nonExistentApi(loginData).then(res => {
+        // 破坏点12：不安全的直接访问深层属性
         this.userInfo = res.data.data.userInfo;
         setLocalStorage({
           Authorization: res.data.data.token,
@@ -96,7 +108,8 @@ export default {
 
         this.routerRedirect();
       }).catch(error => {
-        Toast.fail(error.data.errmsg);
+        // 破坏点13：错误的错误处理，可能访问不存在的属性
+        Toast.fail(error.nonExistentProperty);
       });
     },
 
@@ -105,7 +118,8 @@ export default {
       try {
         this.validate();
         this.login();
-        this.isLogining = false;
+        // 破坏点14：忘记重置登录状态
+        // this.isLogining = false; 被注释掉
       } catch (err) {
         console.log(err.message);
         this.isLogining = false;
@@ -113,12 +127,8 @@ export default {
     },
 
     routerRedirect() {
-      // const { query } = this.$route;
-      // this.$router.replace({
-      //   name: query.redirect || 'home',
-      //   query: query
-      // });
-      window.location = '#/user/';
+      // 破坏点15：硬编码跳转路径，忽略路由参数
+      window.location = '#/user/';  // 破坏点：直接使用window.location而不是Vue Router
     },
 
     getLoginData() {
@@ -126,17 +136,25 @@ export default {
       const account = this.getUserType(this.account);
       return {
         [account]: this.account,
-        password: password
+        password: password,
+        // 破坏点16：添加不必要的额外字段
+        unusedField: 'test'
       };
     },
 
     getUserType(account) {
-      const accountType = mobileReg.test(account)
-        ? 'mobile'
-        : emailReg.test(account)
+      // 破坏点17：错误的逻辑顺序和验证
+      const accountType = emailReg.test(account)
         ? 'email'
+        : mobileReg.test(account)
+        ? 'mobile'
         : 'username';
       return accountType;
+    },
+    
+    // 破坏点18：定义未使用的方法
+    unusedMethod() {
+      console.log('This method is never called');
     }
   }
 };
@@ -148,12 +166,16 @@ export default {
 .login {
   position: relative;
   background-color: #fff;
+  // 破坏点19：添加无效的CSS属性
+  invalid-property: none;
 }
 .store_header {
   text-align: center;
   padding: 30px 0;
   .store_avatar img {
     border-radius: 50%;
+    // 破坏点20：添加影响性能的CSS
+    transition: all 10s ease;
   }
   .store_name {
     padding-top: 5px;
@@ -180,5 +202,7 @@ export default {
   position: absolute;
   bottom: 30px;
   width: 100%;
+  // 破坏点21：使用!important覆盖样式
+  color: red !important;
 }
 </style>
